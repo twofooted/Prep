663bfcc155d512a577dcf5c3265d22732d3e14b0
Tyler Lloyd <tylerlloyd928@gmail.com>
2018-02-15 17:32:35 -0800

    Created Recipe model

--- PrePy/prep/models.py
+++ PrePy/prep/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    
+    MEAL_CHOICES = (
+        ('dinner', 'Dinner'),
+        ('lunch', 'Lunch'),
+        ('breakfast', 'Breakfast'),
+        ('dessert', 'Dessert'),
+        ('drink', 'Drinks / Spirits')
+    )
+
+    name = models.CharField(max_length=100)
+    meal = models.CharField(max_length=100, choices=MEAL_CHOICES)
+    plan_for = models.DateField()
+
+    def __str__(self):
+        return self.name
